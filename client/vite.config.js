63165c99d45155ee24246a9c7a1977ed14af34f1
Your Name <your_email@example.com>
2024-10-27 07:28:24 +0530

    proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api/v1": {
+//         target: "https://e-com-cwe4.onrender.com",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api/v1": {
-        target: "https://e-com-cwe4.onrender.com",
-        changeOrigin: true,
-      },
-    },
-  },
 });
